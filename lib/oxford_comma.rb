4ed35c9611fc88f3524c942a71111d40f2de449d
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 18:06:57 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -11,7 +11,7 @@ def oxford_comma(array)
       new_array.push(ele)
       count += 1 
     end 
-    oxford_array = new_array + new_array[-1]
+    oxford_array = new_array + array[-1]
     return oxford_array.join
   end 
 end
