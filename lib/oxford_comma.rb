8d89caaad19f8932df9b839395599848f716dc0b
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 18:22:21 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     count = 0
     new_array = []
     while count < array.length - 1
-      ele = array[count] + ", "
+      ele = array[count] + ","
       new_array.push(ele)
       count += 1 
     end 
