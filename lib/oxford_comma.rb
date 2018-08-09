d54565e630caeace2c0947957910cb127db26baf
Robert Snyder <r.snyder49@gmail.com>
2018-08-09 17:35:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,9 @@
 def oxford_comma(array)
-  if array.length < 2 
+  if array.length == 1 
+    array.join
+  elsif array.length < 2 
     array.join(" and ")
   elsif array.length > 2 
-    array.each 
+    array.join(", ")
+  end 
+end
